9cda79348642872204dcdee4ae303fb0d4c69db2
claudio-py <101283035+claudio-py@users.noreply.github.com>
2023-06-24 18:01:12 -0300

    updat

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -10,3 +10,30 @@ acordeonTriggers.forEach((trigger) => {
     }
   })
 })
+const swiper = new Swiper(".swiper", {
+	// Optional parameters
+	direction: "horizontal",
+	loop: true,
+slidesPerView: 1,
+	// If we need pagination
+	pagination: {
+		el: ".swiper-pagination"
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev"
+	},
+
+	scrollbar: {
+		el: '.swiper-scrollbar'
+	}
+})
+// ========MODAL=========
+function openModal() {
+	document.getElementById("myModal").style.display = "block"
+}
+function closeModal() {
+	document.getElementById("myModal").style.display = "none"
+}
