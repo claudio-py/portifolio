24d1fd903f62df0a47281d2fb0cd8fd837f6355c
claudio-py <101283035+claudio-py@users.noreply.github.com>
2023-06-24 20:10:10 -0300

    updat3

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -16,9 +16,9 @@ const swiper = new Swiper(".swiper", {
 	loop: true,
 slidesPerView: 1,
 	// If we need pagination
-	pagination: {
-		el: ".swiper-pagination"
-	},
+	// pagination: {
+	// 	el: ".swiper-pagination"
+	// },
 
 	// Navigation arrows
 	navigation: {
